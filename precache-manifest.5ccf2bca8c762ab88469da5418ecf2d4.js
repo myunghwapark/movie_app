7d737e1098eb7d68b3ee3ade849e50a31975674b
Myunghwa <minghoa@naver.com>
2019-09-24 16:07:14 +1200

    Updates

---
+++ precache-manifest.5ccf2bca8c762ab88469da5418ecf2d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfb44d37a2a6cd61735711e6d52e8ca",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0bf0362794e71528434c",
+    "url": "/movie_app/static/css/main.5f3aa185.chunk.css"
+  },
+  {
+    "revision": "2d2d02a4d2cbae94b693",
+    "url": "/movie_app/static/js/2.733aa7db.chunk.js"
+  },
+  {
+    "revision": "0bf0362794e71528434c",
+    "url": "/movie_app/static/js/main.93b032ed.chunk.js"
+  },
+  {
+    "revision": "0a5c0ad77459cf3396ae",
+    "url": "/movie_app/static/js/runtime~main.ebbd9dda.js"
+  }
+]);
